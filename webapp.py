b495a272279af03e51e93a51838f48499210a4c4
Aswin Periyadan Kadinjapali <aswin123@tamu.edu>
2019-05-15 16:42:45 -0500

    UI changes and public server access modifications at port 5000

--- webapp.py
+++ webapp.py
@@ -1,6 +1,5 @@
 # !/usr/bin/env python
 import time
-
 from flask import Flask, render_template,request
 from flask_socketio import SocketIO, emit
 from random import randint
@@ -17,7 +16,10 @@ global a
 A0 = 0
 A1 = 1
 A2 = 2
-PIN = 13
+Power_PIN = 13
+Pressure_OUT = 12
+Temperature_OUT = 11
+
 def background_thread():
     """Example of how to send server generated events to clients."""
     while True:
@@ -27,7 +29,7 @@ def background_thread():
         kt = a.analog_read(A2)
         bd = 20
         socketio.emit('my_response',
-                      {'data': 'Values','heaterTemp': heaterTemp, 'fr': pressure, 'kt': kt, 'bd': bd},
+                      {'data': 'Values','heaterTemp': heaterTemp, 'inPressure': pressure, 'kt': kt, 'bd': bd},
                       namespace='/carpi')
 
 
@@ -53,28 +55,34 @@ def test_connect():
 
 @socketio.on('power button press', namespace='/carpi')
 def handle_message(message):
-    a.set_pin_mode(PIN, 'O')
+    a.set_pin_mode(Power_PIN, 'O')
     time.sleep(1)
     if message.get('checkbox'):
         print("power ON")
-        a.digital_write(PIN, 1)
+        a.digital_write(Power_PIN, 1)
     else:
         print("power OFF")
-        a.digital_write(PIN, 0)
+        a.digital_write(Power_PIN, 0)
 
 
 @socketio.on('temperature input', namespace='/carpi')
 def handle_slider(message):
-    print(message.get('value'))
+    temperature = int(message.get('value'))
+    print(temperature)
+    value = int((temperature / 800) * 255)
+    a.analog_write(Temperature_OUT, value)
 
 
 @socketio.on('pressure input', namespace='/carpi')
 def handle_slider(message):
-    print(message.get('value'))
+    pressure = int(message.get('value'))
+    print(pressure)
+    value = int((pressure/160)*255)
+    a.analog_write(Pressure_OUT, value)
 
 
 if __name__ == '__main__':
-    print("startin application")
+    print("startin application...")
     # a = Arduino(serial_port='COM10')
-    time.sleep(3)
-    socketio.run(app, debug=True)
+    # time.sleep(3)
+    socketio.run(app, host='0.0.0.0',debug=False)
